4001d5dac5706088819f7085823e1021a33cf84d
zj565061763 <565061763@qq.com>
2023-01-15 21:56:37 +0800

    1.0.0-alpha03

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -27,7 +27,7 @@ afterEvaluate {
                 from(components["java"])
                 groupId = "com.sd.lib.android"
                 artifactId = "service-compiler"
-                version = "1.0.0-alpha02"
+                version = "1.0.0-alpha03"
             }
         }
     }
